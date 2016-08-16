5da434cbefb26a328f495fb5459efe3b1eae3f9e
mattheo <mattheo@ulozisko.sk>
2016-08-16 23:49:57 +0200

    do not save history for proxied migrations

--- MigrateController.php
+++ MigrateController.php
@@ -25,7 +25,33 @@ class MigrateController extends \yii\console\controllers\MigrateController
         $this->beforeAction(\Yii::$app->requestedAction);
     }
 
-    public function migrateUp($class)           { return parent::migrateUp($class); }
+    /**
+     * Upgrades with the specified migration class.
+     * @param string $class the migration class name
+     * @return boolean whether the migration is successful
+     */
+    public function migrateUp($class)
+    {
+        if ($class === self::BASE_MIGRATION) {
+            return true;
+        }
+
+        $this->stdout("*** applying $class\n", Console::FG_YELLOW);
+        $start = microtime(true);
+        $migration = parent::createMigration($class);
+        if ($migration->up() !== false) {
+            $time = microtime(true) - $start;
+            $this->stdout("*** applied $class (time: " . sprintf('%.3f', $time) . "s)\n\n", Console::FG_GREEN);
+
+            return true;
+        } else {
+            $time = microtime(true) - $start;
+            $this->stdout("*** failed to apply $class (time: " . sprintf('%.3f', $time) . "s)\n\n", Console::FG_RED);
+
+            return false;
+        }
+    }
+
     public function migrateDown($class)         { return parent::migrateDown($class); }
     public function migrateToTime($time)        { return parent::migrateToTime($time); }
     public function migrateToVersion($version)  { return parent::migrateToVersion($version); }
